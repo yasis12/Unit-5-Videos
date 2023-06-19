31cb8cb55a06b9e4cbe3c8fb2d1381771b4859ef
Rafael Yasis <rafaelyasis@gmail.com>
2023-06-18 20:21:59 -0500

    finished object literals video

--- main.js
+++ main.js
@@ -5,3 +5,20 @@ let card0 = {
     suit: 'Hearts',
     number: 2
 } //end card0
+
+let card1 = {
+    suit: 'Spades',
+    number: 1
+} //end card1
+
+function isRed ( cardToCheck ){
+    console.log( 'in isRed:', cardToCheck);
+    if(cardToCheck.suit === 'Hearts' || cardToCheck.suit === 'Diamonds'){
+        return true;
+    } else {
+        return false;
+    } //end !red
+} //end isRed
+
+console.log(isRed (card0));
+console.log(isRed (card1));
